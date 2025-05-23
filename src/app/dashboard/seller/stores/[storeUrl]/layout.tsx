52ad8c9a31b3ab8bcf43f36b6cf63b148a6f7e32
Lee Acevedo <91152882+Zirodev1@users.noreply.github.com>
2025-05-23 15:32:15 -0500

    Refactor layout in SellerStoreDashboard to remove unnecessary padding and improve responsiveness. Adjusted the layout structure for better alignment of header and content sections.

--- src/app/dashboard/seller/stores/[storeUrl]/layout.tsx
+++ src/app/dashboard/seller/stores/[storeUrl]/layout.tsx
@@ -109,9 +109,9 @@ export default async function SellerStoreDashboardLayout({
     return (
       <div className="h-full w-full flex">
         <Sidebar isDemo demoRole="SELLER" stores={demoStores} />
-        <div className="w-full pl-[280px] pr-4 md:pr-6 lg:pr-8">
+        <div className="w-full pr-4 md:pr-6 lg:pr-8">
           <Header isDemo />
-          <div className="w-full max-w-5xl mx-auto mt-[75px] px-2 sm:px-4 md:px-6 py-4">
+          <div className="w-full mx-auto mt-[75px] px-2 sm:px-4 md:px-6 py-4">
             {children}
           </div>
         </div>
